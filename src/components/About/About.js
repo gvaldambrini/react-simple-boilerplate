edc7310e456e9d58b3efafb88d7dbee4bd81cc2b
Gianni Valdambrini <aleister@develer.com>
2019-04-01 11:58:48 +0200

    Use react hooks for the About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,42 +1,39 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 import styles from "./About.module.scss";
 
-class About extends Component {
-  componentDidMount() {
-    if (!this.props.loaded) {
-      this.props.fetchContributors();
-    }
+const About = props => {
+  const { loaded, fetchContributors } = props;
+  useEffect(() => {
+    if (!loaded) fetchContributors();
+  }, [loaded, fetchContributors]);
+
+  if (!props.loaded) {
+    return null;
   }
 
-  render() {
-    if (!this.props.loaded) {
-      return null;
-    }
-
-    const contributors = this.props.contributors.map(el => (
-      <div key={el.username} className={styles.contributor}>
-        <img src={el.avatar} alt="" />
-        <div>
-          <a href={el.url}>{el.username}</a>
-          <div className={styles.commits}>
-            {el.contributions > 1
-              ? `${el.contributions} commits`
-              : `${el.contributions} commit`}
-          </div>
+  const contributors = props.contributors.map(el => (
+    <div key={el.username} className={styles.contributor}>
+      <img src={el.avatar} alt="" />
+      <div>
+        <a href={el.url}>{el.username}</a>
+        <div className={styles.commits}>
+          {el.contributions > 1
+            ? `${el.contributions} commits`
+            : `${el.contributions} commit`}
         </div>
       </div>
-    ));
+    </div>
+  ));
 
-    return (
-      <div className={styles.about}>
-        <h2>Contributors:</h2>
-        <div>{contributors}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={styles.about}>
+      <h2>Contributors:</h2>
+      <div>{contributors}</div>
+    </div>
+  );
+};
 
 About.propTypes = {
   fetchContributors: PropTypes.func.isRequired,
